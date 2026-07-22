cf34516b87b2dc0d708d12af742c9f6dc701b8ea
Ruppel <12001000014557@fedidcard.gov>
2026-07-22 17:03:33 -0500

    Wed Updates

--- labs/lab06-rest-client/scripts/app.js
+++ labs/lab06-rest-client/scripts/app.js
@@ -1,6 +1,9 @@
 import * as http from './http.js';
 import * as view from './view.js';
 const GET_TRIVIA = "https://opentdb.com/api.php?amount=1&difficulty=easy";
+const BIN_ID = "6a613c05f5f4af5e29b2fd9e";
+const GET_LEADERBOARD = "https://apiljsonbin.io/v3/b/6a613c05f5f4af5e29b2fd9e/latest";
+
 //const state = {};
 const state = {
     score: 0,
@@ -8,27 +11,70 @@ const state = {
     intervalID: null,
     trivia: null
 }
-const playGame = async () => {
-        const json = await http.sendGETRequest(GET_TRIVIA);
-        // console.log(json);
-        [state.trivia] = json.results;
-        view.PlayScene(state.trivia);
-    };
+// const playGame = async () => {
+//         const json = await http.sendGETRequest(GET_TRIVIA);
+//         // console.log(json);
+//         [state.trivia] = json.results;
+//         view.PlayScene(state);
+// }
+window.playGame = async () => {
+    const json = await http.sendGETRequest(GET_TRIVIA);
+    console.log("json from API:", json);
+
+    if (!json || !json.results || json.results.length === 0) {
+        console.error("No trivia results found:", json);
+        return;
+    }
+
+
+    [ state.trivia ] = json.results;
+    view.PlayScene(state);
+}
 
 const countdown = ()=> {
-    if (state.timer) {
+    console.log("Timer:",state.timer);
+    if (state.timer > 0) {
         state.timer--;
         view.PlayScene(state);
         //view.PlayScene({timer,score,trivia});
+    } else {
+        clearInterval(state.intervalID);
+        view.GameOverScene(state);
     }
 }
 
-const createGame = () => {
+// const createGame = () => {
+//     state.score = 0;
+//     state.timer = 20;
+//     clearInterval(state.intervalID);
+//     state.intervalID = setInterval(countdown, 1000);
+//     playGame();
+// }
+window.createGame = () => {
+    state.score = 0;
     state.timer = 20;
-    state.intervalID = setInterval(countdown, 1000);
+    clearInterval(state.intervalID);
+    state.intervalID = setInterval(countdown,1000);
     playGame();
 }
+
 window.start = async () => {
-    createGame;
+    // createGame();
+    state.score = 0;
+    state.timer = 20;
+    view.StartMenu(state);
 }
-window.addEventListener("load", playGame);
+
+window.checkAnswer = (attempt) => {
+    const answer = state.trivia.correct_answer;
+    if (attempt === answer) {
+        state.score += state.timer;
+        state.timer += 10;
+        playGame();
+    } else {
+        clearInterval(state.intervalID);
+        view.GameOverScene(state);
+    }
+}
+//window.addEventListener("load", playGame);
+window.addEventListener("load",createGame);
--- labs/lab06-rest-client/scripts/view.js
+++ labs/lab06-rest-client/scripts/view.js
@@ -1,9 +1,11 @@
 import Question from './components/Question.js';
 import HUD from './components/HUD.js';
+import Skip from './components/Skip.js';
 
 const renderDOM = (html) => { 
     document.getElementById("view").innerHTML = html; 
 };
+
 export const PlayScene = (props) => {
 //    const trivia = props;
     const {timer,score,trivia} = props;
@@ -11,6 +13,16 @@ export const PlayScene = (props) => {
 //        ${Question(trivia)}
 //            Question(props)
         `${HUD(timer,score)}
-        ${Question(trivia)}`
+        ${Question(trivia)}
+        ${Skip()}`
+    )
+};
+
+export const GameOverScene = (props) => {
+    const {timer,score,trivia} = props;
+    renderDOM(
+        `${HUD(timer,score)}
+        <h1>Game Over!</h1>
+        <button onclick="start()">Start Over</button>`
     )
 };
